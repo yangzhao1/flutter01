bd68d7f617a5ee7c81bb7c5449c2ed483dca1d84
vstx143 <549890753@qq.com>
2019-08-16 16:26:24 +0800

    go

--- yy_project_manage/lib/pages/manage_page.dart
+++ yy_project_manage/lib/pages/manage_page.dart
@@ -1,29 +1,134 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-class YYManagePage extends StatefulWidget{
+
+class YYManagePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return YYManageStatePage();
   }
 }
 
-class YYManageStatePage extends State<YYManagePage>{
+class YYManageStatePage extends State<YYManagePage> {
+  // final Map<String, List> _items = {
+  //   '审批节点上报管理': [
+  //     {'title': '上报信息', 'image': ''},
+  //   ],
+  //   '替办管理':[{'title':''}]
+  // };
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-       body:  new Swiper(
-        itemHeight: 150,
-        itemWidth: 100,
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
+      appBar: AppBar(
+        title: Text('管理'),
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          _createSliverHeader('审批节点上报管理'),
+          _createCell(),
+          _createSliverHeader('督办管理'),
+          _createCell(),
+        ],
       ),
     );
   }
 
+  //section header
+  SliverPersistentHeader _createSliverHeader(String title) {
+    return SliverPersistentHeader(
+      delegate: GradHeader(title),
+    );
+  }
+
+  // cell
+  SliverPadding _createCell() {
+    return SliverPadding(
+      padding: EdgeInsets.all(5),
+      sliver: SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          mainAxisSpacing: 10,
+          childAspectRatio: 1,
+        ),
+        delegate: SliverChildBuilderDelegate(
+          (BuildContext context, int index) {
+            //创建子widget
+            return _createItemCell();
+          },
+          childCount: 4,
+        ),
+      ),
+    );
+  }
+
+  Container _createItemCell() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.ac_unit,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            '主题',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+//
+class UnitItemModel<T> {
+  UnitItemModel(this.title,this.imageName,this.className);
+  String title;
+  String imageName;
+  T className; 
+}
+//
+class GradHeader extends SliverPersistentHeaderDelegate {
+  GradHeader(this.title);
+
+  final String title;
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      height: 30,
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 5, bottom: 5),
+            width: 4,
+            color: Colors.blue,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 30;
+  @override
+  double get minExtent => 30;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
 }
